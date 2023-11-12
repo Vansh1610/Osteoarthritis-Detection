eb91fe2aa2a9606529e1d882bbae80c65fb137c7
Vansh1610 <vansh.jain162000@gmail.com>
2023-11-12 13:52:49 -0800

    requirements done

--- setup.py
+++ setup.py
@@ -1,2 +1,33 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description=f.read()
 
 
+__version__='0.0.0'
+
+REPO_NAME="Osteoarthritis-Detection"
+AUTHOR_USER_NAME='Vansh1610'
+SRC_REPO='Osteoarthritis Classifier'
+AUTHOR_EMAIL='vansh.jain162000@gmail.com'
+
+
+setuptools.setup(
+
+
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description='A python package for CNN APP',
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        'Bug Tracker':f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+
+)
